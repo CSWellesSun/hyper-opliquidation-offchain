89115121024b65f3665ee278c131922e1541d47b
CSWellesSun <3200102339@zju.edu.cn>
2026-01-18 15:57:29 +0100

    feat: split

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/big"
+	"os"
+	"strconv"
+
+	"github.com/CSWellesSun/hypermevlib/mev"
+	"github.com/CSWellesSun/hypermevlib/utils"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <command>")
+	}
+
+	switch os.Args[1] {
+	case "split":
+		// go run main.go split <splitAddressAmount> <targetAmount> <sendValue>
+		client, err := mev.NewClient(os.Getenv("HYPE_RPC_URL"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		splitAddressAmount, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		targetAmount, err := strconv.ParseFloat(os.Args[3], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sendValue, err := strconv.ParseFloat(os.Args[4], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pk, err := utils.GetPrivateKey(os.Getenv("HYPE_REPLENISH_PK"))
+		if err != nil {
+			log.Fatal("Error getting private key")
+		}
+		eoa := utils.GetAddress(pk)
+		splitPks, err := utils.GetPrivateKeysFromMnemonic(os.Getenv("MNEMONIC"), splitAddressAmount)
+		if err != nil {
+			log.Fatal("Error getting private keys from mnemonic")
+		}
+		splitAddresses := make([]common.Address, splitAddressAmount)
+		for i := range splitPks {
+			splitAddresses[i] = utils.GetAddress(splitPks[i])
+		}
+		nonce, err := client.NonceAt(context.Background(), eoa, nil)
+		if err != nil {
+			log.Fatal("Error getting pending nonce")
+		}
+		for i := 0; i < len(splitAddresses); i += 50 {
+			end := i + 50
+			if end > len(splitAddresses) {
+				end = len(splitAddresses)
+			}
+			splitAddressesBatch := splitAddresses[i:end]
+			if i != 0 {
+				sendValue = 0
+			}
+			tx, err := mev.CallSplitContract(
+				eoa,
+				nonce,
+				pk,
+				common.HexToAddress(os.Getenv("SPLIT_CONTRACT_ADDRESS")),
+				splitAddressesBatch,
+				utils.ToWei(big.NewFloat(targetAmount), "ether"),
+				utils.ToWei(big.NewFloat(sendValue), "ether"),
+				client,
+			)
+			if err != nil {
+				log.Fatalf("Error calling split contract: %v", err)
+			}
+			log.Printf("Split contract call sent: %s", tx.Hash().Hex())
+			nonce++
+		}
+	default:
+		log.Fatalf("Unknown command: %s", os.Args[1])
+	}
+}
